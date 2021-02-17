c2776bba6c7e706d73bab46e1b61d585cab4ff68
Jared Zook <jaredzook@gmail.com>
2021-02-16 18:30:39 -0800

    removed vue-tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/vue-tailwind.js'],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -58,6 +58,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['vue-tailwind']
+    transpile: []
   }
 }
